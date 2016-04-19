ea3df3af4b4565835c177a1b3ecec774efd869f3
Angor <angor@inbox.ru>
2016-04-19 12:27:09 +0300

    на поле раставляются кирпичи

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject brick;
+    // Use this for initialization
+	void Start () {
+        Screen.SetResolution(600, 900, false, 60);
+        fillTheFild();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void fillTheFild ()
+    {
+        for (int i=-4; i<5; i++)
+        {
+            for (int k=1; k<10; k++)
+            {
+                Instantiate(brick, new Vector3 (i*2.1f,k*1.1f,0.0f), Quaternion.identity);
+            }
+        }
+    }
+}
