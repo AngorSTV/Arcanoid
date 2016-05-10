e4be79e63f99a0d08ad57cc2162020586c8c97d6
Андрей <a.gornostaev@informika.ru>
2016-05-10 16:52:55 +0300

    управление на Андроид сделано по любой части экрана

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,11 +12,24 @@ public class PlayerController : MonoBehaviour
     public float xMax;
     private Rigidbody rb;
     private float carrentY;
+    private GameObject score;
+    private int screenWidth;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         carrentY = rb.position.y;
+
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Score");
+        score = gos[0];
+        /*Component[] allComponents;
+        allComponents = score.GetComponents<Component>();
+        foreach (Component component in allComponents)
+        {
+            //Debug.Log(component.GetType());
+        }*/
+        screenWidth = Screen.currentResolution.width;
+        Debug.Log(Screen.currentResolution.width);
 
     }
 
@@ -27,16 +40,19 @@ public class PlayerController : MonoBehaviour
         Touch[] touches = Input.touches;
         foreach (Touch touch in touches)
         {
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
             {
-                if (touch.position.x > 0)
+                //score.GetComponent<UnityEngine.UI.Text>().text = "x=" + touch.rawPosition.x.ToString() + " y=" + touch.position.y.ToString();
+                if (touch.position.x > screenWidth / 2)
                     moveHorizontal = 1;
-                if (touch.position.x < 0)
+                if (touch.position.x < screenWidth / 2)
                     moveHorizontal = -1;
             }
         }
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        
+        //score.GetComponent<UnityEngine.UI.Text>().text = moveHorizontal.ToString();
         rb.velocity = movement * speed;
         rb.position = new Vector3(Mathf.Clamp(rb.position.x, xMin, xMax), carrentY, 0.0f);
         rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
